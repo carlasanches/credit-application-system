2f66b20fe89a42bff012faa3c0dabbe47c9877f1
carlasanches <carlasanches8@gmail.com>
2023-10-21 22:00:12 -0300

    Create entity and repository classes

---
+++ src/main/kotlin/com/example/credit/application/system/repository/CreditRepository.kt
@@ -0,0 +1,8 @@
+package com.example.credit.application.system.repository
+
+import com.example.credit.application.system.entity.Credit
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface CreditRepository: JpaRepository<Credit, Long>
